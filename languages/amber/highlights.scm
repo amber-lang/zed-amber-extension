5fa340f148e35101d0367d8f05ae99696159aaff
Phoenix Himself <pkaras.it@gmail.com>
2024-07-11 16:43:55 +0200

    feat: update syntax highlighter

--- languages/amber/highlights.scm
+++ languages/amber/highlights.scm
@@ -1,42 +1,45 @@
 (comment) @comment
 
-; [
-;     "if"
-;     "loop"
-;     "ref"
-;     "return"
-;     "fun"
-;     "else"
-;     "then"
-;     "break"
-;     "continue"
-;     "and"
-;     "or"
-;     "not"
-;     "let"
-;     "pub"
-;     "main"
-;     "echo"
-;     "fun"
-;     "import"
-;     "as"
-;     "in"
-;     "fail"
-;     "failed"
-;     "status"
-;     "silent"
-;     "nameof"
-;     "is"
-;     "unsafe"
-; ] @keyword
+[
+    "if"
+    "loop"
+    "return"
+    "fun"
+    "else"
+    "then"
+    "break"
+    "continue"
+    "and"
+    "or"
+    "not"
+    "let"
+    "pub"
+    "main"
+    "echo"
+    "fun"
+    "import"
+    "as"
+    "in"
+    "fail"
+    "failed"
+    "silent"
+    "nameof"
+    "is"
+    "unsafe"
+] @keyword
 
 ; Literals
 (boolean) @constant.builtin.boolean
 (number) @constant.numeric
 (null) @constant.numeric
 (string) @string
-(status) @keyword
+; (status) @keyword
 (command) @string
+(handler) @keyword
+(block) @delimeter
+(variable_init) @keyword
+(variable_assignment) @delimiter
+(variable) @variable
 (escape_sequence) @constant.character.escape
 ; (function_definition
 ;     name: (variable) @function.method)
